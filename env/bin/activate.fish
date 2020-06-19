8db57964c2bac7f881b56c7f079ddf67ee3fc250
Dane Santos <dsantos@umass.edu>
2020-06-18 22:55:29 -0400

    virtual environment , django

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/nolanlarochelle/Downloads/Sharendar/env"
+set -gx VIRTUAL_ENV "/Users/danesantos/Desktop/Sharendar/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
