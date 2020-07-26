00e10c0ac9760557c0aa966fc713ed1526b4dbd5
Aayush <lamblesus@gmail.com>
2020-07-26 19:50:29 -0400

    some more setup

--- sharendarapp/backend/views.py
+++ sharendarapp/backend/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import * 
 
 def home(request):
-    return HttpResponse("<h1> kung pow penis </h1>")
+    events = Events.objects.all()
+
+    context = {
+        'events': events
+    }
+
+    return render(request, 'backend/list.html', context)
 
 # Create your views here.
