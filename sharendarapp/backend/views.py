57a8b7610093887ef6a4b1aa35e4affc825d0c59
Aayush <lamblesus@gmail.com>
2020-07-26 21:54:47 -0400

    create event function

--- sharendarapp/backend/views.py
+++ sharendarapp/backend/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import * 
+from .forms import *
 
 def home(request):
     events = Events.objects.all()
 
+    form = EventForm()
+
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
     context = {
-        'events': events
+        'events': events,
+        'form': form
     }
 
     return render(request, 'backend/list.html', context)
 
-# Create your views here.
+
