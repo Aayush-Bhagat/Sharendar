c7368bfa0272f56bc43ebb172e021c329fd9e7e1
Aayush <lamblesus@gmail.com>
2020-08-05 15:04:06 -0400

    fixed models

--- sharendarapp/backend/models.py
+++ sharendarapp/backend/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 # Create your models here.
 class Events(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length = 100)
-    start_date = models.DateField(default=None)
-    end_date = models.DateField(default=None)
-    start_time = models.TimeField(blank=True, null=True)
-    end_time = models.TimeField(blank=True, null=True)
+    start_time = models.DateTimeField( default = timezone.now)
+    end_time = models.DateTimeField( default= timezone.now)
     description = models.CharField(max_length = 300, blank=True)
     
     def __str__(self):
